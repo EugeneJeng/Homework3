39bea0267c70281dd88aea719acd81261d2c5bf5
Eugene <whale7829@gmail.com>
2022-02-11 15:53:06 +0800

    Add Say Hi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Monkey!");
+            Console.WriteLine("Hello Human!");
             Console.ReadLine();
         }
     }
